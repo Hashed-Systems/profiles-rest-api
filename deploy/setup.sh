23109c750d57f743028ee180469ad76595e5ba53
Hafiz Zain <onlyyzain@gmail.com>
2020-05-16 01:51:14 +0400

    remote git changes

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/hafizzain/api-standards.git'
+PROJECT_GIT_URL='https://github.com/Hashed-Systems/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
